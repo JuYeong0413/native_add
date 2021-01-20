2b623d3753fc51adeb91d5f2b54f29bf7d38f9b4
Juyeong Lee <2jy22@naver.com>
2021-01-21 00:47:44 +0900

    load the code using the FFI library

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -3,6 +3,18 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+import 'dart:ffi'; // For FFI
+import 'dart:io'; // For Platform.isX
+
+final DynamicLibrary nativeAddLib = Platform.isAndroid
+    ? DynamicLibrary.open("libnative_add.so")
+    : DynamicLibrary.process();
+
+final int Function(int x, int y) nativeAdd =
+  nativeAddLib
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("native_add")
+    .asFunction();
+
 class NativeAdd {
   static const MethodChannel _channel =
       const MethodChannel('native_add');
